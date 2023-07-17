1cf6edc994f26ccddd268b7196fc3be960c1a4de
Giuseppe Tomis <giuseppe.tomis@protonmail.com>
2023-07-17 17:56:53 +0200

    sitemap v2

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -1,20 +1,24 @@
 import { GetServerSideProps } from "next"
 
 export const getServerSideProps: GetServerSideProps = async ({ res }) => {
-  const urls = ["/"]
-
   let xml = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`
 
-  urls.forEach((url) => {
-    xml += `
+  xml += `
       <url>
-        <loc>${`https://www.petomis.com${url}`}</loc>
+        <loc>${`https://www.petomis.com`}</loc>
         <lastmod>${new Date().toISOString()}</lastmod>
         <changefreq>monthly</changefreq>
         <priority>1.0</priority>
       </url>`
-  })
+
+  xml += `
+      <url>
+        <loc>${`https://petomis.com`}</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <changefreq>monthly</changefreq>
+        <priority>1.0</priority>
+      </url>`
 
   xml += `</urlset>`
 
