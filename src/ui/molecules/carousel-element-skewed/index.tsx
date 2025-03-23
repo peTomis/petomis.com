aecf660d302a5572636e2b268388ebe8bb599ce0
Giuseppe <giuseppe.tomis@protonmail.com>
2025-03-23 15:18:53 +0100

    fix: ios element skewed overflow

--- src/ui/molecules/carousel-element-skewed/index.tsx
+++ src/ui/molecules/carousel-element-skewed/index.tsx
@@ -16,7 +16,7 @@ const CarouselElementSkewed = ({
   return (
     <div className={`flex relative`} onMouseEnter={onMouseEnter}>
       <div
-        className={`flex overflow-hidden z-10 ${
+        className={`flex overflow-visible z-10 ${
           hovered
             ? ""
             : "w-[40px] sm:w-[60px] sm:max-w-[100px] lg:w-[100px]  skew-y-6 opacity-60"
