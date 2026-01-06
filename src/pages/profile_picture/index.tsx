a43318fae4c4ba77f01a826547bc67e4c657db8b
Giuseppe Tomis <giuseppe.tomis@protonmail.com>
2026-01-06 18:12:30 +0100

    feat: blue profile_picture assets

--- src/pages/profile_picture/index.tsx
+++ src/pages/profile_picture/index.tsx
@@ -1,8 +1,13 @@
 import image from "@public/images/profile_picture.png"
+import image2 from "@public/images/profile_picture_2.png"
 import link from "@public/images/link.png"
+import link2 from "@public/images/link_2.png"
 import map from "@public/images/map.png"
+import map2 from "@public/images/map_2.png"
 import phone from "@public/images/phone.png"
+import phone2 from "@public/images/phone_2.png"
 import email from "@public/images/mail.png"
+import email2 from "@public/images/mail_2.png"
 import Image from "next/image"
 
 import { NextPage } from "next"
@@ -44,6 +49,46 @@ const ProfilePicture: NextPage = () => {
       />
       <Image
         src={email}
+        alt="Mail icon"
+        loading="eager"
+        priority={true}
+        width={50}
+        height={50}
+      />
+      <Image
+        src={image2}
+        alt="Profile picture"
+        loading="eager"
+        priority={true}
+        width={1000}
+        height={1000}
+      />
+      <Image
+        src={link2}
+        alt="Link icon"
+        loading="eager"
+        priority={true}
+        width={50}
+        height={50}
+      />
+      <Image
+        src={map2}
+        alt="Map icon"
+        loading="eager"
+        priority={true}
+        width={50}
+        height={50}
+      />
+      <Image
+        src={phone2}
+        alt="Phone icon"
+        loading="eager"
+        priority={true}
+        width={50}
+        height={50}
+      />
+      <Image
+        src={email2}
         alt="Mail icon"
         loading="eager"
         priority={true}
